00b782d59a87a262ad4b23c7af541aa50abb2ff2
Shouchuang Zhu <zscjsw@gmail.com>
2019-06-09 19:49:13 -0500

    shorthand and destructure

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,13 +2,13 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/1d9c60ae33d322ef76a3047e0375567e/' + latitude + ',' + longitude;
-    request({url: url, json: true}, (error, response) =>{
+    request({url: url, json: true}, (error, {body}) =>{ //response replaced by body
         if(error){
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error){
+        } else if (body.error){
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degree out. This is a ' + response.body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degree out. This is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 
