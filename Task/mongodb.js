17404cf5ea9c2bc60bd68c808bdd9b3787bce54d
Shouchuang Zhu <zscjsw@gmail.com>
2019-06-13 19:36:16 -0500

    fetch

--- Task/mongodb.js
+++ Task/mongodb.js
@@ -1,25 +1,23 @@
 //CRUD create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
+const id = new ObjectID()
+console.log(id.getTimestamp())
 MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client)=>{
     if(error){
         return console.log('Unable to connect to database!')
     }
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Shouchuang',
-        age: 25
-    }, (error, result)=>{
+    db.collection('users').findOne({ name: 'Jen' }, (error, user)=>{
         if(error){
-            return console.log('Unable to insert user')
+            return console.log('Unable to fetch')
         }
-        console.log(result.ops)
+        console.log(user)
     })
-
+   
 })
